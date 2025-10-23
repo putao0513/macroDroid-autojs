1cad15c83e8d687bc173b9e736370c95bfa3bd8c
putao0513 <2734322488@qq.com>
2025-10-23 09:17:46 +0800

    Create 发送天气预报.js

---
+++ macrodroid/发送天气预报.js
@@ -0,0 +1,16 @@
+
+2025年10月23日09:16:08
+这个感觉是个伪需求， 安卓的macrodroid无法实现发送微信，即便通过曲折的发送邮箱， 微信接收邮箱形式，无法显示内容，并不适用于需求场景， 苹果快捷指令
+可以轻松实现，目前无需再花费时间精力捣鼓，知道了方法和渠道即可
+彩云天气的api调用 可以使用多个账号来获取1w的调用次数，嘿嘿
+获取天气api
+解析文本
+
+彩云天气api管理平台
+https://platform.caiyunapp.com/application/manage
+爱博一村经纬度
+31.20869, 121.30631
+
+curl "https://api.caiyunapp.com/v2.6/TAkhjf8d1nlSlspN/116.3176,39.9760/weather?alert=true&dailysteps=1&hourlysteps=24"
+"location": [39.2072, 101.6656], // 请求经纬度
+  101.6656,39.2072
